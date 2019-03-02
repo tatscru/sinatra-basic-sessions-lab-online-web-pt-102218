028572594febf0951564683944ce585bb04ed520
tatiana.mojica <tatiana.mojica@bustle.com>
2019-03-02 19:39:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   configure do
-  enable :sessions
-  set :session_secret, "secret"
-end
+    enable :sessions
+    set :session_secret, "secret"
+  end
 end
